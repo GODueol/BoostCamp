e4893687970d9cacc5c9c23420ffddae90a6a1cb
kj <kj@sweettracker.co.kr>
2018-12-12 18:51:25 +0900

    [수정] 1. 수정쬐끔

--- app/src/main/java/com/android/godueol/boostcamp/MovieSearchViewModel.kt
+++ app/src/main/java/com/android/godueol/boostcamp/MovieSearchViewModel.kt
@@ -1,36 +1,32 @@
 package com.android.godueol.boostcamp
 
 import android.annotation.SuppressLint
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.databinding.ObservableArrayList
 import com.android.godueol.boostcamp.model.MovieInfo
 import com.android.godueol.boostcamp.repository.MovieAPI
 import com.android.godueol.boostcamp.utlis.RxBinder
 import com.android.godueol.boostcamp.utlis.threadIoToMain
 import com.google.gson.Gson
-import org.json.JSONObject
-import java.net.URLEncoder
-import android.text.Editable
-import android.text.TextWatcher
-import android.content.Intent
-import android.net.Uri
 import io.reactivex.Observable
-import io.reactivex.subjects.PublishSubject
-import java.util.concurrent.TimeUnit
-import javax.security.auth.Subject
-import io.reactivex.ObservableEmitter
 import io.reactivex.ObservableOnSubscribe
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
+import org.json.JSONObject
+import java.net.URLEncoder
+import java.util.concurrent.TimeUnit
 
 
 class MovieSearchViewModel(private val rxBinder: RxBinder) {
     var items = ObservableArrayList<MovieInfo>()
-    var searchword : String? = null
+    var searchword: String? = null
 
     @SuppressLint("CheckResult")
-    fun requestMovies(searchWord:String){
+    fun requestMovies(searchWord: String) {
         MovieAPI.instance!!.mAPI
             .getMovieList(URLEncoder.encode(searchWord, "UTF-8"))
             .compose(threadIoToMain())
@@ -41,7 +37,7 @@ class MovieSearchViewModel(private val rxBinder: RxBinder) {
                 {
                     val jsonObject = JSONObject(it)
                     val jsonArray = jsonObject.getJSONArray("items")
-                    Log.d("size",""+jsonArray.length())
+                    Log.d("size", "" + jsonArray.length())
                     for (i in 0 until jsonArray.length()) {
                         val item = Gson().fromJson(jsonArray[i].toString(), MovieInfo::class.java)
                         items.add(item)
@@ -50,27 +46,26 @@ class MovieSearchViewModel(private val rxBinder: RxBinder) {
                     Log.e("fail", it.message)
                 })
     }
-    fun onSearch(v: View,word:String) {
+
+    fun onSearch(v: View, word: String?) {
+        word ?: run {
+            Toast.makeText(v.context,"검색어를 입력해 주세요.",Toast.LENGTH_SHORT).show()
+            return
+        }
         requestMovies(word)
     }
 
-    fun goUrl(v:View, url:String){
-        val intent = Intent(Intent.ACTION_VIEW)
-        val uri = Uri.parse(url)
-        intent.data = uri
-        v.context.startActivity(intent)
-    }
     var watcher: TextWatcher = object : TextWatcher {
         override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
 
         override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-           searchword = s.toString()
+            searchword = s.toString()
         }
 
         @SuppressLint("CheckResult")
         override fun afterTextChanged(s: Editable) {
 
-            if(s.isBlank()||s.length<2)
+            if (s.isEmpty() || s.length < 2)
                 return
 
             Observable.create(ObservableOnSubscribe<String> { emitter ->
@@ -80,7 +75,7 @@ class MovieSearchViewModel(private val rxBinder: RxBinder) {
                 .subscribeOn(Schedulers.io())
                 // Be notified on the main thread
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe{
+                .subscribe {
                     items.clear()
                     requestMovies(it)
                 }
--- app/src/main/java/com/android/godueol/boostcamp/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/android/godueol/boostcamp/adapter/RecyclerViewAdapter.kt
@@ -1,8 +1,11 @@
 package com.android.godueol.boostcamp.adapter
 
+import android.content.Intent
+import android.net.Uri
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.Constraints
 import androidx.recyclerview.widget.RecyclerView
 import com.android.godueol.boostcamp.model.MovieInfo
 import com.android.godueol.boostcamp.R
@@ -18,6 +21,19 @@ class RecyclerViewAdapter : RecyclerView.Adapter<BaseViewHolder<MovieListRowBind
 
     override fun onBindViewHolder(holder: BaseViewHolder<MovieListRowBinding>, position: Int) {
         holder.binding?.item = itemList[position]
+        holder.binding?.itemLayout?.setOnClickListener { v->
+                val intent = Intent(Intent.ACTION_VIEW)
+                val uri = Uri.parse(itemList[position].link)
+                intent.data = uri
+                v.context.startActivity(intent)
+        }
+
+
+        val barSize = (itemList[position].userRating*30).toInt()
+        Log.d("barSize up:", itemList[position].userRating.toString())
+        Log.d("barSize : ",barSize.toString())
+        val layoutParams=Constraints.LayoutParams(barSize,50)
+        holder.binding?.movieRatingBar?.layoutParams = layoutParams
     }
 
 
